528580071b45e1821e69e3fd0cdfce53caf56024
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-07-26 15:43:51 +0530

    added controller, model and routes to split

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+const createCurrentUser = async (req: Request, res: Response) => {};
+
+export default { createCurrentUser };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
+import UserRoutes from "./routes/UserRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Connected to MongoDB");
@@ -12,11 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({
-    message: "test",
-  });
-});
+app.use("/api/my/user", UserRoutes);
 
 app.listen(8000, () => {
   console.log("Server started on http://localhost:8000");
