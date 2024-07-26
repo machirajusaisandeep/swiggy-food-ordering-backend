d0347fa608953221c524a00a49b5bad1559a7780
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-07-26 15:55:52 +0530

    created first api for create user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,35 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
-const createCurrentUser = async (req: Request, res: Response) => {};
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      auth0Id,
+      name,
+      email,
+      addressLine1,
+      city,
+      state,
+      country,
+      pincode,
+    } = req.body;
+
+    const existingUser = await User.findOne({ auth0Id });
+    if (existingUser) {
+      return res.status(200).json({
+        message: "User already exists",
+      });
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error creating user",
+    });
+  }
+};
 
 export default { createCurrentUser };
--- src/models/user.ts
+++ src/models/user.ts
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   auth0Id: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
