8df1141007ee17380244f5afc3431f252fa86650
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-07-26 11:14:08 +0530

    added backend to database connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
 app.use(express.json());
